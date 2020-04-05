b0425fbe6b457aa07a58c217b6cea51996147a3f
Yann Tavernier <yann.tavernier@qima.com>
2020-04-05 14:47:51 +0200

    chore: lint all and lint on save

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,78 +1,91 @@
 import colors from 'vuetify/es5/util/colors';
 
-const base = 'gh-pages' === process.env.NODE_ENV ? '/nuxt-resume/' : '/';
+const base = process.env.NODE_ENV === 'gh-pages' ? '/nuxt-resume/' : '/';
 
 export default {
-  mode: 'spa',
-  /*
-   ** Headers of the page
-   */
-  head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-  },
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: ['@nuxt/typescript-build', '@nuxtjs/vuetify'],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [],
-  router: {
-    base
-  },
-  /*
-   ** vuetify module configuration
-   ** https://github.com/nuxt-community/vuetify-module
-   */
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
-  },
-  /*
-   ** Build configuration
-   */
-  build: {
+    mode: 'spa',
     /*
-     ** You can extend webpack config here
+     ** Headers of the page
      */
-    publicPath: '/static/',
-    extend(config, ctx) {}
-  }
-}
+    head: {
+        titleTemplate: '%s - ' + process.env.npm_package_name,
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: process.env.npm_package_description || ''
+            }
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    },
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: '#fff' },
+    /*
+     ** Global CSS
+     */
+    css: [],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: ['@nuxt/typescript-build', '@nuxtjs/vuetify'],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [],
+    router: {
+        base
+    },
+    /*
+     ** vuetify module configuration
+     ** https://github.com/nuxt-community/vuetify-module
+     */
+    vuetify: {
+        customVariables: ['~/assets/variables.scss'],
+        theme: {
+            dark: true,
+            themes: {
+                dark: {
+                    primary: colors.blue.darken2,
+                    accent: colors.grey.darken3,
+                    secondary: colors.amber.darken3,
+                    info: colors.teal.lighten1,
+                    warning: colors.amber.base,
+                    error: colors.deepOrange.accent4,
+                    success: colors.green.accent3
+                }
+            }
+        }
+    },
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        publicPath: '/static/',
+        extend(config, ctx) {
+            // Exécuter ESLint lors de la sauvegarde
+            if (ctx.isDev && ctx.isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                });
+            }
+        }
+    }
+};
